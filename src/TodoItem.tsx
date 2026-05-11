38edcabcbfaef7166a7e2aaa9061258f4505ba75
AK-Gedeon <gedeonadabadji3@gmail.com>
2026-05-11 08:36:11 +0200

    Todo list avec Reactjs

---
+++ src/TodoItem.tsx
@@ -0,0 +1,56 @@
+import { Trash } from "lucide-react";
+
+type Priority = "Urgente" | "Moyenne" | "Base";
+    
+type Todo = {
+    id: number;
+    text: string;
+    priority: Priority;
+}    
+
+type Props = {
+    todo: Todo
+    onDelete: () => void
+    isSelected : boolean
+    onToggleSelect : (id : number) => void
+}
+
+const TodoItem = ({todo, onDelete, isSelected, onToggleSelect} : Props) => {
+
+    return(
+        <li className="p-3 ">
+            <div className="flex justify-between items-center">
+                <div className="flex item-center gap-2">
+                    <input
+                     type="checkbox"
+                      className="checkbox checkbox-primary checkbox-sm"
+                      checked={isSelected}
+                      onChange={() => onToggleSelect(todo.id)}
+                    />
+                    <span className="text-md font-bold">
+                        <span>{todo.text}</span>
+                        <span
+                        className={`badge badge-sm badge-soft
+                            ${
+                                todo.priority == "Urgente" ?
+                                 "badge-error"
+                                 : todo.priority == "Moyenne" ?
+                                 "badge-warning"
+                                 : "badge-success"
+
+                            }`}
+                        >
+                            {todo.priority}
+                        </span>
+                    </span>
+                </div>
+                <button
+                 onClick={onDelete}
+                  className="btn btn-sm btn-error btn-soft">
+                    <Trash className="w-4 h-4"/>
+                </button>
+            </div>
+        </li>
+    )
+}
+export default TodoItem
